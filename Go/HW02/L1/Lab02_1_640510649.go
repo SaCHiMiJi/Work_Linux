7f94b15933986a9121aab3c1e0c9ce5343ff7eec
SaCHIMiJi <junjao800@gmail.com>
2022-07-08 12:39:34 +0700

    commit

--- Go/HW02/L1/Lab02_1_640510649.go
+++ Go/HW02/L1/Lab02_1_640510649.go
@@ -1,10 +1,9 @@
-// ชื่อ นามสกุล - ชื่อเล่น
-// 6XXXXXXXX
+// ชื่อ ชัยนันท์ เรืองกจวณิชกุล
+// 640510649
 // Lab02_1
-// 204203 Sec 00B
+// 204203 Sec 001
 
 package main
-
 
 func powerOfTwo(x uint64) bool {
 	return x-(x&(x-1)) == x
