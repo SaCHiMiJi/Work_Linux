6ed592a531a5591c51e64734e7dd9387e84289b4
SaCHIMiJi <junjao800@gmail.com>
2022-08-13 14:59:28 +0700

    commit

--- Python/lab08/Lab08_4.py
+++ Python/lab08/Lab08_4.py
@@ -1,33 +1,59 @@
 
-def median_of_median(list_a:list, i:int=0, list_b:list = [])->float:
-    if len(list_a) == 2:
-        num_s = sum(list_a)/2
-        return num_s
-    if len(list_a) == 1:
-        return list_a[0]
-    
-    elif i//3 == len(list_a)//3:
-        
-        list_a = list_b
-        return median_of_median(list_a, 0, [])
-    else:
-        
-        if len(list_a)%3 != 0 and i == len(list_a)//3*3-3:
-            over_list = sum(list_a[i+2:])/(len(list_a)%3+1)
-            list_c = list_a[i:i+2] + [over_list]
-            sum_list = sum(list_c) 
-            sum_list -= max(list_c)+ min(list_c)
-            
+from operator import le
+
+
+def median_of_median(list_a: list) -> float:
+    # print("list_a : ", list_a)
+
+    n = len(list_a) // 3
+    # print("n :", n)
+    if len(list_a) < 3:
+        return mid_num(list_a)
+    if n == 1:
+        if len(list_a) % 3 == 0:
+            # print(list_a[i:])
+            mid_n = list_a[0:]
+            # print(mid_n)
         else:
-            sum_list = sum(list_a[i:i+3])
-            sum_list -= max(list_a[i:i+3])+ min(list_a[i:i+3])
-            
-        list_b.append(sum_list)
-        return median_of_median(list_a, i+3, list_b)
+            # print("in")
+            list_x = list_a[2:]
+            # print("extra_list : ", list_x)
+            exta = sum(list_x)/len(list_x)
+            # print(list_a[0:2]+[exta])
+            # print("extra_num :", exta)
+            mid_n = list_a[0:2]+[exta]
+
+        return mid_num(mid_n)
+    else:
+        list_b = []
+        mid_n1 = median_of_median(list_a[0:n*(1)])
+        mid_n2 = median_of_median(list_a[n:n*(2)])
+        mid_n3 = median_of_median(list_a[n*2:])
+        list_b += [mid_n1]+[mid_n2]+[mid_n3]
+        # print(list_b)
+        return mid_num(list_b)
+
+
+def mid_num(list_num: list):
+    # print("list_num :",list_num)
+    if len(list_num) == 2:
+        num = sum(list_num)/2
+        return num
+    if len(list_num) == 1:
+        return list_num[0]
+    else:
+        list_num.remove(max(list_num))
+        list_num.remove(min(list_num))
+        return mid_num(list_num)
+
 
 def main():
-    list_a = [28, 14,  13,  21,  19,  27,  23,  30,  16,  31, 1]
+    list_a = [28, 14, 13, 22, 20, 23, 17, 18, 19, 16, 15, 12, 13, 18, 20]
+    print(len([1, 2, 3, 4, 5.5]))
     print(median_of_median(list_a))
+    print(median_of_median([28, 14, 13, 21, 20, 23, 17, 18, 19, 16, 15, 12]))
+    print(median_of_median([1, 2, 3]))
+    print(median_of_median([28, 14, 13, 22, 10]))
 
 
 if __name__ == '__main__':
--- Python/lab08/Lab08_5.py
+++ Python/lab08/Lab08_5.py
@@ -1,42 +1,47 @@
 
 
 def medal_allocation(list_a: list, result: list = []) -> list:
-    list_a = list_a.sort()
-    num1 = list_a.count(list_a[0])
+    print(list_a)
+    list_b = list_a.sort(reverse=True)
+    print(list_b)
+    num1 = list_b.count(list_b[0])
     # print(num1)
-    result.append(list_a[0:num1])
-    if list_a[0] == 0:
+    result.append(list_b[0:num1])
+    if list_b[0] == 0:
         result = [[], [], []]
     elif num1 >= 3:
         result.append([])
         result.append([])
     else:
-        # print(list_a[num1])
-        num2 = list_a.count(list_a[num1])
-        if list_a[num2+num1-1] == 0:
+        # print(list_b[num1])
+        num2 = list_b.count(list_b[num1])
+        if list_b[num2+num1-1] == 0:
             
             result.append([])
             result.append([])
         elif num1 == 2:
             
             result.append([])
-            result.append([list_a[num1]])
-        elif num2+num1 >= 2:
-            
-            result.append(list_a[num1:num2+num1])
+            result.append([list_b[num1]])
+        elif num2 >= 2:
+            # print("in")
+            result.append(list_b[num1:num2+num1])
             result.append([])
         else:
-            if list_a[num2+num1-1] == 0:
+            
+            if list_b[num2+num1] == 0:
+                
                 result.append([])
             else:
-                result.append(list_a[num1:num2+num1])
-                num3 = list_a.count(list_a[num2+num1])
-                result.append(list_a[num1+num2:num3+num2+num1])
-    return result
+                
+                result.append(list_b[num1:num2+num1])
+                num3 = list_b.count(list_b[num2+num1])
+                result.append(list_b[num1+num2:num3+num2+num1])
+    return tuple(result)
 
 
 def main():
-    list_a = [9, 8, 8,  7, 7, 7, 6, 5, 4, 3, 2]
+    list_a = [9, 8, 7, 7, 7, 6, 5, 4, 3, 2]
     print(medal_allocation(list_a))
 
 
